5026060ac27dfb6bc439a3751a8834fe405596f0
sugiyamatoshiki <s.toshiki.llwz8869@gmail.com>
2019-10-01 13:26:49 +0900

    install webpack-dev-server

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import _ from 'lodash';
 function component(){
     const element = document.createElement('div');
-    const array = ['Hello', 'webpack'];
+    const array = ['Hello', 'webpack', '!'];
     // lodash はアンスコを使う
     element.innerHTML = _.join(array, ' ');
     return element;
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,12 @@
-const path = require('path')
-const outputPath = path.resolve(__dirname, 'dist')
+const path = require('path');
+const outputPath = path.resolve(__dirname, 'dist');
 module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'main.js',
-    path: outputPath
-  }
+    path: outputPath,
+  },
+  devServer: {
+    contentBase: outputPath,
+  },
 }
